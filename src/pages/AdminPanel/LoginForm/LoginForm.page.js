3290766ab6f9fa24d7c2a86fe0a75812d4f90f83
Mahdi Khavari <mkhavari813@gmail.com>
2021-06-19 22:30:24 +0430

    add the component to route

---
+++ src/pages/AdminPanel/LoginForm/LoginForm.page.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+class LoginForm extends Component{
+    render(){
+        return(
+            <h1>Login form</h1>
+        )
+    }
+}
+
+export {LoginForm}
--- src/route/App.route.js
+++ src/route/App.route.js
@@ -2,6 +2,7 @@ import { Component } from "react";
 import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom';
 // import pages in here =>
 import {SiteHeader} from '../layout/index'
+import {LoginForm} from '../pages/AdminPanel/LoginForm/LoginForm.page'
 //
 class AppRoute extends Component{
     render(){
@@ -12,6 +13,11 @@ class AppRoute extends Component{
                         <SiteHeader>
                             <h1>main part of website will be here</h1>
                         </SiteHeader>
+                    </Route>
+                </Switch>
+                <Switch>
+                    <Route path="/adminLogin" exact>
+                        <LoginForm></LoginForm>
                     </Route>
                 </Switch>
             </BrowserRouter>
