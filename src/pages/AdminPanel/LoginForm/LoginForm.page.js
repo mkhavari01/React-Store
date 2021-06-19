4c51134a47f9eaa9354587de503a531804a7aab3
Mahdi Khavari <mkhavari813@gmail.com>
2021-06-19 23:20:41 +0430

    add ui and responsivity

--- src/pages/AdminPanel/LoginForm/LoginForm.page.js
+++ src/pages/AdminPanel/LoginForm/LoginForm.page.js
@@ -1,9 +1,35 @@
 import { Component } from "react";
-
+import {Form,Button} from 'react-bootstrap'
+import styled from './LoginForm.module.css'
+import { Link } from "react-router-dom";
 class LoginForm extends Component{
     render(){
         return(
-            <h1>Login form</h1>
+            <div className={styled.container}>
+                <Form className={styled.form}>
+                    <h1 className={styled.formTitle}>
+                        ورود به پنل مدیریت فروشگاه فلان
+                    </h1>
+                    <div className={styled.inputGroups}>
+                        <Form.Group controlId="formBasicEmail" style={{width:'100%'}}>
+                            <Form.Label className={styled.label}>نام کاربری :</Form.Label>
+                            <Form.Control type="email" />
+                        </Form.Group>
+
+                        <Form.Group controlId="formBasicPassword" style={{width:'100%'}}>
+                            <Form.Label className={styled.label}>رمز عبور :</Form.Label>
+                            <Form.Control type="password" />
+                        </Form.Group>
+
+                        <Button variant="none" type="submit" className={styled.loginBtn}>
+                            ورود
+                        </Button>
+                    </div>
+                    <Link to="/" style={{position: 'absolute','bottom': '15px',left: '15px',fontWeight:'bold'}}>
+                        بازگشت به سایت
+                    </Link>
+                </Form>
+            </div>
         )
     }
 }
