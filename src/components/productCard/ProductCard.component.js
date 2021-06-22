17b5473589035c6e8776b747d972be6811721a0f
Mahdi Khavari <mkhavari813@gmail.com>
2021-06-22 12:21:35 +0430

    add cards and ui fixing

--- src/components/productCard/ProductCard.component.js
+++ src/components/productCard/ProductCard.component.js
@@ -1,12 +1,22 @@
 import { Component } from "react";
 import styled from './productCard.module.css';
-
+import pictures from '../../assets/food.svg'
 class ProductCard extends Component{
     render(){
         return(
-            <div>
-                <h1>hello world</h1>
-            </div>
+                <div className={styled.wrapper}>
+                    <div className={styled.imgContainer}>
+                        <img width='85%' height='85%' src={pictures}/>
+                    </div>
+                    <div className={styled.productDetail}>
+                        <h2>
+                        در کادر زیر هر متنی را که دوست دارید 
+                        </h2>
+                        <h2>
+                            ۲۰۰ هزار تومان
+                        </h2>
+                    </div>
+                </div>
         )
     }
 }
--- src/pages/site/Home/Home.page.js
+++ src/pages/site/Home/Home.page.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import {ProductCard} from '../../../components/index'
+import styled from './Home.page.module.css'
 class Home extends React.Component{
     render(){
         return(
             <>
+            <h1 className={styled.h1}>کالاهای گروه فست فود</h1>
+            <div className='container-fluid d-flex justify-content-around mt-1 flex-wrap'>
                 <ProductCard />
+                <ProductCard />
+                <ProductCard />
+            </div>
+                
             </>
         )
     }
