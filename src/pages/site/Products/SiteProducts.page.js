f5b343bf8b25e0e61b237a2b8166249ce0f1af6f
Mahdi Khavari <mkhavari813@gmail.com>
2021-07-03 10:47:50 +0430

    add db.json and filtering for products page

--- src/pages/site/Products/SiteProducts.page.js
+++ src/pages/site/Products/SiteProducts.page.js
@@ -2,11 +2,20 @@ import React from 'react'
 import styled from './products.module.css';
 import {SiteHeader} from '../../../layout/index'
 import axios from 'axios';
-
+import {ProductCard} from '../../../components/index'
 class SiteProducts extends React.Component{
-    state={
-        sideBar : []
+    constructor(props){
+        super(props)
+
+        this.state={
+            sideBar : [],
+            mainProducts :[],
+            filteredProducts : []
+        }
+
+        this.linkHandler = this.linkHandler.bind(this)
     }
+    
     componentDidMount(){
         axios.get('http://localhost:3000/sideBar')
          .then((res)=>{
@@ -15,12 +24,23 @@ class SiteProducts extends React.Component{
          })
          axios.get('http://localhost:3000/products')
          .then((res)=>{
-             const products = res.data
-             this.setState({products : products})
+             const mainProducts = res.data
+             this.setState({
+                mainProducts : mainProducts ,
+                filteredProducts : mainProducts
+             })
          })
     }
     linkHandler(event){
-        console.log(event.target.textContent)
+        const filteredProducts = this.state.mainProducts.filter(f => f.subGroup==event.target.textContent)
+        // console.log(t)
+        // let bigCities = cities.filter(city => city.population > 3000000);
+        this.setState({
+            filteredProducts : filteredProducts
+        })
+        // setTimeout(() => {
+        //     console.log(this.state.filteredProducts)
+        // }, 2000);
     }
     render(){
         const renderSidebar = this.state.sideBar.map((el) => {
@@ -29,19 +49,30 @@ class SiteProducts extends React.Component{
                         {el.name}
                     </ul>
                     {el.subNames.map((subnames)=>{
-                        return <li> <a onClick={this.linkHandler} href='#'> {subnames} </a> </li>
+                        return <li> <a onClick={this.linkHandler} href={"#"+subnames}>{subnames}</a> </li>
                     })}
+                    </>
+        });
+        // this.state.products.length = 6
+        const renderProducts = this.state.filteredProducts.map((el) => {
+            return <>
+                    {<ProductCard 
+                        price={el.price+'هزار تومان'} 
+                        desc={el.name}
+                        picture={el.avatar}
+                        key={el.id}
+                    />}
                     </>
         });
         return(
             <>
             <SiteHeader />
-            <div className={styled.sideBar}>
+            <section className={styled.sideBar}>
                 {renderSidebar}
-            </div>
-            <div className={styled.products}>
-                main part
-            </div>
+            </section>
+            <section className={styled.products}>
+                {renderProducts}
+            </section>
             </>
         )
     }
